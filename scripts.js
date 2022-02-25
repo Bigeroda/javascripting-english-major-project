21142aea3dce4308da813f10c6e78acd3a1b3912
Bigeroda <82051341+Bigeroda@users.noreply.github.com>
2022-02-25 09:30:02 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 alert("scripts.js has loaded!"); 
 let userInput;
-userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
-if ( userInput === "burrito" ) {
+userInput = prompt("Whats your favorite color", "Blue.");
+if ( userInput === "Blue." ) {
   $("#response").html("Brilliant choice!");
 } else {
-  $("#response").html("Don’t you want a burrito?");
+  $("#response").html("What about Blue?");
 }
 
