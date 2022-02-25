41f912b51053a2faec39edc8f67fa6104f39f140
Bigeroda <82051341+Bigeroda@users.noreply.github.com>
2022-02-25 09:32:46 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ alert("scripts.js has loaded!");
 let userInput;
 userInput = prompt("Whats your favorite color", "Blue.");
 if ( userInput === "Blue." ) {
-  $("#response").html("Brilliant choice!");
+  $("#response").html("Wonderful choice!");
 } else {
   $("#response").html("What about Blue?");
 }
