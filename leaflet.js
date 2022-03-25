d38ca0a47271079c0abe1d218f07908d6a63aa59
Bigeroda <82051341+Bigeroda@users.noreply.github.com>
2022-03-25 10:17:36 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -11,4 +11,19 @@ firstMap.setView([40.730833, -73.9975], 16);
 
 let washingtonSquarePark, washingtonSquareParkMarker, bobstLibrary, bobstLibraryMarker;
 washingtonSquarePark = L.latLng(40.730833, -73.997222);
-washingtonSquareParkMarker =
+washingtonSquareParkMarker = L.marker(washingtonSquarePark).bingPopup("This is Washington Square Park.").addTo(firstMap);
+bobstLibrary = L.latLng(40.729444, -73.997222);
+bobstLibrary = L.marker(bobstLibrary).bindPopup("This is Bobst Library.").addTo(firstMap);
+let circle;
+circle = L.circle(washingtonSquarePark, {radius: 100,
+                                         color: "#EF4824",
+                                         fillColor: "#EF4824
+                                         opacity: 0.9,
+                                         fillOpacity: 0.20}
+                  ).addTo(firstMap);
+circle = L.circle(bobstLibrary,         {radius: 100,
+                                         color: "#EF4824",
+                                         fillColor: "#EF4824"
+                                         opacity: 0.9,
+                                         fillOpacity: 0.20}
+                  ).addTo(firstMap);
